27fd2bf3a63c39182a9c55c6163299c866f35971
Haniel2607 <hanielschramm@gmail.com>
2024-09-24 22:42:40 -0300

    banco de dados

---
+++ Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvoZFLx-BtcvFVPSKH44VuWXm0STu7zaM",
+  authDomain: "appjogadores.firebaseapp.com",
+  projectId: "appjogadores",
+  storageBucket: "appjogadores.appspot.com",
+  messagingSenderId: "271375955815",
+  appId: "1:271375955815:web:b3ef3f07816feb80c421c2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
